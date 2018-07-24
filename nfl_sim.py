a6c43cad712b69e5399914bac115df4f161c5749
Alex <ajcav2@gmail.com>
2018-07-23 20:01:56 -0500

    Added method to validate team while the user adds players. This ensures the user doesn't accidentally add too many players of one type which would be against the rules.

--- nfl_sim.py
+++ nfl_sim.py
@@ -1,19 +1,43 @@
 import nflgame
 
+
 def get_team():
     players = []
     new_player = raw_input('Enter player name: ')
     while new_player:
         new_player = validate_player(new_player)
         if new_player:
-            players.append(new_player)
+            if validate_team(players, new_player):
+                players.append(new_player)
         else:
             print('Could not find player. Please try again.')
         new_player = raw_input('Enter player name: ')
     print('Your team is: ')
     for player in players:
-        print(player.full_name)
+        print(player.full_name+', '+player.team)
     return players
+
+
+def validate_team(players, addition):
+    team_restrictions = {'QB': 4,
+                         'RB': 8,
+                         'WR': 8,
+                         'TE': 3,
+                         'K': 3,
+                         'D/ST': 3}
+
+    potential_team = players + [addition]
+    if len(potential_team) >= 16:
+        print('Cannot add player. Team is already full.')
+        return False
+
+    for player in potential_team:
+        if player.position in team_restrictions.keys():
+            team_restrictions[player.position] -= 1
+            if team_restrictions[player.position] < 0:
+                print('Cannot add another '+player.position)
+                return False
+    return True
 
 
 def validate_player(player):
@@ -22,10 +46,10 @@ def validate_player(player):
         return matches[0]
     elif len(matches) == 0:
         return None
-    print('Found '+str(len(matches))+' for '+player+'.')
+    print('Found '+str(len(matches))+' matches for '+player.title()+'.')
     print('Please select from the following options.')
     for i in range(len(matches)):
-        print('('+str(i+1)+') - '+player+', '+matches[i].team+', number '+str(matches[i].number))
+        print('('+str(i+1)+') - '+player.title()+', '+matches[i].team+', number '+str(matches[i].number))
     selection = int(raw_input('Enter your selection: '))
     return matches[selection-1]
 
